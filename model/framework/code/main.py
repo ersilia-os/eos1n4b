5bce6e49de20873e9f76f6206ecd22b5af7e2efe
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-12-18 22:54:17 +0100

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -48,20 +48,14 @@ def predict_class(input_file, model_pre, output_file):
     df_input = pd.read_csv(input_file)
     df_pre = Compute(df_input)
     X = np.array(df_pre)
-    class_pre = model_pre.predict(X)
     proba_pre = model_pre.predict_proba(X)
-    list_active   = [x[1] for x in proba_pre]
-    list_inactive = [x[0] for x in proba_pre]
-    df_pre.insert(0,'SMILES',df_input.iloc[:, 0])
-    df_pre.insert(1,'class_pre',class_pre)
-    df_pre.insert(2,'Probability_active',list_active)
-    df_pre.insert(3,'Probability_inactive',list_inactive)
+    list_active = [x[1] for x in proba_pre]
 
-    df_pre['Bioactivity_pre'] = df_pre['class_pre'].apply(class_type2)
+    df_result = pd.DataFrame({
+        'Probability_active': list_active
+    })
 
-    df_pre = df_pre[['SMILES','Bioactivity_pre','Probability_active']]
-    df_pre_final = df_pre.sort_values(by="Probability_active",ascending= False)  
-    df_pre_final.to_csv(output_file,index=None)
+    df_result.to_csv(output_file, index=None)
 
 # parse arguments
 input_file = sys.argv[1]
