5bbf52b0de3e768ec262195371c7651c6f78249c
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-12-20 15:20:51 +0100

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -12,6 +12,17 @@ from rdkit.Chem import AllChem
 from rdkit.Chem import MolFromSmiles
 from rdkit.Chem.AllChem import GetMorganFingerprintAsBitVect
 
+# parse arguments
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+# current file directory
+root = os.path.dirname(os.path.abspath(__file__))
+
+# load pre-trained model
+model_path= os.path.abspath(os.path.join(root,"..", "..","checkpoints", "XGBoost_Morgan2.m"))
+model_pre = joblib.load(model_path)
+
 # Functions
 def Fingerprint_compute(Smi):
     try:
@@ -22,49 +33,42 @@ def Fingerprint_compute(Smi):
     except:
         return 'error'
 
-def Compute(df):
-    length = len(df)
-    List_smiles = df.iloc[:, 0]
-
+def Compute(smiles_list): #pass smiles list
     List_fp = []
-    for i in range(0,length):
-        smi = List_smiles[i]
-        if smi == 'error':
-            continue
-        else:
-            fp = Fingerprint_compute(smi)
-            fp_split = list(str(fp))
-            List_fp.append(fp_split)
+    for i in range(0,len(smiles_list)):
+        smi = smiles_list[i] 
+        fp = Fingerprint_compute(smi)
+        fp_split = list(str(fp))
+        List_fp.append(fp_split)
     df_fp = pd.DataFrame(List_fp)
     return df_fp
 
-def class_type2(class_pre):
-    flag = 'Inactive'
-    if class_pre == 1:
-        flag = 'Active'
-    return flag
-
-def predict_class(input_file, model_pre, output_file):
-    df_input = pd.read_csv(input_file)
-    df_pre = Compute(df_input)
+def my_model(smiles_list):
+    df_pre = Compute(smiles_list) #compute expects a list
     X = np.array(df_pre)
     proba_pre = model_pre.predict_proba(X)
     list_active = [x[1] for x in proba_pre]
 
-    df_result = pd.DataFrame({
-        'Probability_active': list_active
-    })
+    return list_active # return the list of predicted probabilities(Outputs)
 
-    df_result.to_csv(output_file, index=None)
 
-# parse arguments
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+# read SMILES from .csv file, assuming one column with header
+with open(input_file, "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+    smiles_list = [r[0] for r in reader]
 
-# current file directory
-root = os.path.dirname(os.path.abspath(__file__))
-# load pre-trained model
-model_path= os.path.abspath(os.path.join(root,"..", "..","checkpoints", "XGBoost_Morgan2.m"))
-model_pre = joblib.load(model_path)
-# predict bioactivity and save results
-predict_class(input_file, model_pre, output_file)
+# run model (# predict bioactivity probability score)
+outputs = my_model(smiles_list)
+
+#check input and output have the same length
+input_len = len(smiles_list)
+output_len = len(outputs)
+assert input_len == output_len
+
+# write output in a .csv file (save result)
+with open(output_file, "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["value"])  # header
+    for o in outputs:
+        writer.writerow([o])
